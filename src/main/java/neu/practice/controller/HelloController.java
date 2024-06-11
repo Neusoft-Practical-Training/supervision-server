72dcd53fc062e4e0816b7a8f484be75d181cb1ee
Techjoy <930763179@qq.com>
2024-06-11 09:43:28 +0800

    project init

---
+++ src/main/java/neu/practice/controller/HelloController.java
@@ -0,0 +1,16 @@
+package neu.practice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello World");
+        return "<h1>Hello World</h1>";
+    }
+}
---
+++ src/test/java/neu/practice/SupervisionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package neu.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
